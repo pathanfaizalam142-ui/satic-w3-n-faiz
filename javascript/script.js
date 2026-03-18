e96735178bbbd52af0a467e592b3bc63f51a8c72
pathan-faiz <pathanfaizalam142@gmail.com>
2026-03-18 18:16:55 +0530

    Add files via upload

---
+++ javascript/script.js
@@ -0,0 +1,62 @@
+// ===== DAY 3 CHART =====
+
+const ctx = document.getElementById('d3Chart');
+
+new Chart(ctx, {
+    type: 'bar',
+
+    data: {
+        labels: [
+            'Faiz',
+            'Rahul',
+            'Aman',
+            'Ali',
+            'John',
+            'Sara',
+            'Karan',
+            'Zoya',
+            'Imran',
+            'Neha'
+        ],
+
+        datasets: [{
+            label: 'Revenue',
+            data: [5200, 4300, 6100, 4800, 7000, 3900, 4500, 5200, 6100, 4700],
+
+            backgroundColor: '#3b82f6',
+            borderRadius: 8,
+            barThickness: 18
+        }]
+    },
+
+    options: {
+        indexAxis: 'y', // horizontal bars
+
+        responsive: true,
+
+        plugins: {
+            legend: {
+                display: false
+            }
+        },
+
+        scales: {
+            x: {
+                ticks: {
+                    color: 'white'
+                },
+                grid: {
+                    color: 'rgba(255,255,255,0.1)'
+                }
+            },
+            y: {
+                ticks: {
+                    color: 'white'
+                },
+                grid: {
+                    display: false
+                }
+            }
+        }
+    }
+});
